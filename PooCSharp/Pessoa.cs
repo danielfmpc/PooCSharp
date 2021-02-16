c660b99467497e7ab0a691cd28cc0b8ca39fa8ea
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-16 03:28:19 -0300

    Instanciando pessoa

---
+++ PooCSharp/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PooCSharp
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
