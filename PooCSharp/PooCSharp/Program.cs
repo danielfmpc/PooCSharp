d3d03f273402a381cf21e13ef708496a9eae9d11
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2021-02-16 02:29:51 -0300

    Iniciando projeto

---
+++ PooCSharp/PooCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PooCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
